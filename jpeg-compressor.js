6cd0250bfb32ce086bf11f680b20f1eac1292e01
Guilherme Serradilha <guibravy@gmail.com>
2016-11-28 12:12:58 -0200

    code style fixes
    Now following standard-js best practices

--- jpeg-compressor.js
+++ jpeg-compressor.js
@@ -1,56 +1,46 @@
 /**
 * @Author: Guilherme Seradilha
 * @Date:   25-Nov-2016 09:27:08
-* @Email:  gserradilha@adagency.com.br
 * @Last modified by:   Guilherme Seradilha
-* @Last modified time: 25-Nov-2016 10:13:48
+* @Last modified time: 28-Nov-2016 12:11:35
 */
 
+'use strict'
 
-"use strict";
-
-var JPEGcompressor = function() {
-
-    self.Run = function(data, quality, callback) {
-
-        if (!isDataURL(data)) {
-            throw 'Image must be encoded as DataURL.';
-            return;
-        }
-
-        if (Number.isNaN(quality) || quality > 1 || quality < 0) {
-            throw 'Quality must be float between 0 and 1.';
-            return;
-        }
-
-        if (typeof callback !== 'function') {
-            throw 'Callback function is required.';
-            return;
-        }
-
-        var img = new Image(),
-            canvas = document.createElement('canvas'),
-            context = canvas.getContext('2d');
-
-        img.onload = function() {
-            canvas.height = img.height;
-            canvas.width = img.width;
-            context.drawImage(img, 0, 0);
-            callback(canvas.toDataURL("image/jpeg", quality));
-        }
-
-        img.src = data;
+var JPEGcompressor = function () {
+  JPEGcompressor.Run = function (data, quality, callback) {
+    // Validations
+    if (!isDataURL(data)) {
+      throw new TypeError('Image must be encoded as DataURL.')
+    }
+    if (Number.isNaN(quality) || quality > 1 || quality < 0) {
+      throw new TypeError('Quality must be float between 0 and 1.')
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError('Callback function is required.')
     }
 
-    function isDataURL(s) {
-        return !!s.match(/^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i);
+    var img = new window.Image()
+    var canvas = document.createElement('canvas')
+    var context = canvas.getContext('2d')
+    img.onload = function () {
+      canvas.height = img.height
+      canvas.width = img.width
+      context.drawImage(img, 0, 0)
+      callback(canvas.toDataURL('image/jpeg', quality))
     }
-
-    return self;
+    img.src = data
+  }
+  // Checks if a string contains a DataURL object
+  function isDataURL (s) {
+    return !!s.match(/^\s*data:([a-z]+\/[a-z]+(;[a-z-]+=[a-z-]+)?)?(;base64)?,[a-z0-9!$&',()*+,;=\-._~:@/?%\s]*\s*$/i)
+  }
+  return JPEGcompressor
 }
 
 if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports)
-        exports = module.exports = JPEGcompressor;
-    exports.JPEGcompressor = JPEGcompressor;
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = JPEGcompressor
+  }
+  exports.JPEGcompressor = JPEGcompressor
 }
